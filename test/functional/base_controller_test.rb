57a3fc9016be90e5420d7d3dc1b830ff5735a4e7
Paco Guzman <pacoguzmanp@gmail.com>
2009-10-24 15:49:04 +0200

    Added engines/plugins model's paths to the build_klasses method

--- test/functional/base_controller_test.rb
+++ test/functional/base_controller_test.rb
@@ -14,4 +14,29 @@ class AdminData::BaseControllerTest < ActionController::TestCase
     end
   end
 
+  context 'testing private method extend_with_plugins_models' do
+    setup do
+      @controller = AdminData::DiagnosticController.new
+      puts "#{@controller.class.filter_chain.inspect}"
+    end
+
+    should 'get awesome model filename from the awesome plugin' do
+      model_names = @controller.send(:extend_with_plugins_models, [])
+      assert model_names.any?
+      assert model_names.include?("awesome.rb")
+    end
+
+    should 'extend only with the new model filename config.rb' do
+      model_names = @controller.send(:extend_with_plugins_models, ["config.rb"])
+      assert_equal 2, model_names.size
+      assert_equal ["config.rb", "awesome.rb"], model_names      
+    end
+
+    should 'extend only if exist new model filenames' do
+      model_names = @controller.send(:extend_with_plugins_models, ["config.rb", "awesome.rb"])
+      assert_equal 2, model_names.size
+      assert_equal ["config.rb", "awesome.rb"], model_names
+    end
+  end
+
 end
