5b40eb33303b80de0abe8b91c7b0deaf10abf84f
Alex Rothenberg <alex@alexrothenberg.com>
2009-10-27 15:17:22 -0400

    allow security check procs to access current model and klass

--- test/functional/base_controller_test.rb
+++ test/functional/base_controller_test.rb
@@ -2,19 +2,7 @@ require 'test/test_helper'
 
 class AdminData::BaseControllerTest < ActionController::TestCase
 
-  context 'testing filter ensure_is_allowed_to_view' do
-    setup do
-      @filter = AdminData::BaseController.before_filter.detect do |filter|
-        filter.kind_of?(ActionController::Filters::BeforeFilter) && filter.method == :ensure_is_allowed_to_view
-      end
-    end
-    should 'have filter called ensure_is_allowed_to_view' do
-      assert @filter
-      assert @filter.options.blank?
-    end
-  end
 
-  should_have_before_filter :ensure_is_allowed_to_view
   should_have_before_filter :build_klasses
   should_have_before_filter :build_drop_down_for_klasses
 
